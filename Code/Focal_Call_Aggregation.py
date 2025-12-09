9cc9a08b6c5345f9aaadc852a93a08a624497513
sameerswarup <sameerswarup77@gmail.com>
2025-12-09 21:57:43 +0800

    Copied over Graham's version of Focal_Call_Aggregation

--- Code/Focal_Call_Aggregation.py
+++ Code/Focal_Call_Aggregation.py
@@ -3,65 +3,31 @@ import numpy as np
 import os
 import pandas as pd
 import glob
-from scipy.io import loadmat
 os.chdir("V:\\MP Data\\Focal Whale Call Data w GPS")
 
-#%% Import Date/Time Data - test commit 
+#%% Import Parent .csv files with Call and Coordinate Data integrated from single files
 
-df = pd.read_csv(
-    "Focal Calls\\bp23_205c_selections_datetime.txt",
-    sep="\t",          # tab-delimited
-    engine="python",   # handles irregular fields safely
-)
+#import Parent Call List and Parent Coordinates
+parent_calls = pd.read_csv("Parent_Call_List.csv")
+parent_coords = pd.read_csv("Parent_Coordinates.csv")
 
-print(df.shape)
-print(df.head())
+# Merge Parent Call List with Parent Coordinates on "Tag" column
+merged_data = pd.merge(parent_calls, parent_coords, on="Tag", how="inner")
 
-#%% Import Focal Call Data
+# Export merged files to CSV
+merged_data.to_csv("Merged_Parents.csv", index=False)
 
-df = pd.read_csv(
-    "Focal Calls\\bp23_205c_selections_focal.txt",
-    sep="\t",          # tab-delimited
-    engine="python",   # handles irregular fields safely
-)
+# %% Filter for Results with Similar Times
 
-print(df.shape)
-print(df.head())
+# Merged csvs bring in two time columns, "time_x" from Parent Call List and "time_y" from Parent Coordinates
+# Convert time columns to datetime objects
+merged_data["Time_x"] = pd.to_datetime(merged_data["Time_x"])
+merged_data["Time_y"] = pd.to_datetime(merged_data["Time_y"])
 
-# %% Aggregate All Focal Call Data
+# Calculate absolute time difference between the two time columns
+merged_data["time_diff"] = (merged_data["Time_x"] - merged_data["Time_y"]).abs()
 
-dfs = []
+filtered_data = merged_data[merged_data["time_diff"] <= pd.Timedelta(minutes=90)]
 
-for file in glob.glob("C:\\Users\\ghg7\\Desktop\\MP Data\\Focal Whale Call Data w GPS\\Focal Calls\\*.txt"):
-    print("Reading:", file)
-
-    df = pd.read_csv(
-        file,
-        sep="\t",
-        engine="python"
-    )
-
-    # enforce 24 columns
-    df = df.iloc[:, :24]
-    dfs.append(df)
-
-combined = pd.concat(dfs, ignore_index=True)
-
-print(combined.shape)
-
-dfs
-
-# %%Getting GPS coordinates from Matlab
-
-mat_data = loadmat('finGPS\\bp23_205c\\bp23_205c001gps.mat')
-print(mat_data.keys())  # shows variable names inside the .mat file
-
-
-# %% Printing variables to look at data (edit to produce coordinates)
-myvar = mat_data['obs'] #prints empty list, troubleshoot
-
-#print(myvar)
-# %%
-pd.DataFrame(myvar[0])
-
-# %%
+# Export filtered results to CSV
+filtered_data.to_csv("Filtered_Parents.csv", index=False)
